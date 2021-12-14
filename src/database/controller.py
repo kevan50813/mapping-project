88ace4dc73e0ae4bd29a4237dfd2aeac19439df6
Thomas Carroll <tcarroll@snufk.in>
2021-12-14 18:51:34 +0000

    Search

--- src/database/controller.py
+++ src/database/controller.py
@@ -11,7 +11,8 @@ class Controller():
 
         Redis database must have Graph and Search modules
     """
-    def __init__(self, host:str, port:str):
+
+    def __init__(self, host: str, port: str):
         self.log = logging.getLogger(__name__)
         self.redis_db = redis.Redis(host=host, port=port)
 
@@ -32,7 +33,7 @@ class Controller():
             self.log.debug("Index does not exist, creating index")
             self.search_client.create_index(schema, definition=definition)
 
-    def save_graph(self, graph_name:str, nodes:list, edges:list) -> None:
+    def save_graph(self, graph_name: str, nodes: list, edges: list) -> None:
         """
             Save a graph given the nodes and edges to the database,
             breadth-first traversal
@@ -43,8 +44,16 @@ class Controller():
                        by graph_parser)
                 edges (list): A list of tuples mapping node id to node id
                        (sparse adjacency matrix)
+
         """
+        # TODO: should we make sure the graph is clear first
         graph = Graph(graph_name, self.redis_db)
+
+        # For full-text search in the graph we need to create an index
+        index_query = """CALL db.idx.fulltext.createNodeIndex
+                         ('node', 'name', 'number')"""
+        graph.query(index_query)
+
         # clean None from dict, redis doesn't understand it
         nodes = [{k: ('' if v is None else v)
                   for k, v in d.items()} for d in nodes]
@@ -55,37 +64,37 @@ class Controller():
         queue.append(nodes[0])
         visited.append(nodes[0])
         while len(queue) != 0:
-            outer_node = queue.pop(0)
+            u = queue.pop(0)
 
             # the alias is there to stop duplication, it needs to start with
             # a letter for some reason
             node1 = Node(label='node',
-                         properties=outer_node,
-                         alias="n"+str(outer_node["id"]))
+                         properties=u,
+                         alias="n"+str(u["id"]))
             graph.add_node(node1)
 
             # TODO I'm sure there's a better way of doing this, ensure
             # bi-directonality
             adjacent = []
-            for edge in edges:
-                if outer_node["id"] == edge[0]:
-                    adjacent.append(edge[1])
-                elif outer_node["id"] == edge[1]:
-                    adjacent.append(edge[0])
+            for e in edges:
+                if u["id"] == e[0]:
+                    adjacent.append(e[1])
+                elif u["id"] == e[1]:
+                    adjacent.append(e[0])
 
             adjn = [n for n in nodes if n["id"] in adjacent]
-            for node in adjn:
-                if node not in visited:
+            for n in adjn:
+                if n not in visited:
                     node2 = Node(label='node',
-                                 properties=node,
-                                 alias="n"+str(node["id"]))
+                                 properties=n,
+                                 alias="n"+str(n["id"]))
                     edge = Edge(node1, 'path', node2)
 
                     graph.add_node(node2)
                     graph.add_edge(edge)
 
-                    visited.append(node)
-                    queue.append(node)
+                    visited.append(n)
+                    queue.append(n)
 
         self.log.debug("commiting graph %s", graph_name)
         graph.commit()
@@ -131,8 +140,16 @@ class Controller():
 
         return pois
 
-    def search_room_nodes(self, search_string) -> list:
+    def search_room_nodes(self, graph_name: str, search_string: str) -> list:
         """
             Search for room nodes by name
         """
-        return []
+        graph = Graph(graph_name, self.redis_db)
+
+        # We should query the graph for two things, both the name and the
+        # room number
+        query = """CALL db.idx.fulltext.queryNodes(node, $search_string)
+                   YIELD node RETURN node.title, node.score"""
+        res = graph.query(query, {"search_string": search_string})
+
+        return res.result_set
