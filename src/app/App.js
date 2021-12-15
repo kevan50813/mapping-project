f14a1ed9425d7e4e5254028188bec8f75ede874f
sc18kj <sc18kj@leeds.ac.uk>
2021-12-15 10:47:49 +0000

    added basic VR stuff to app.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import ArCoreReactNativeViewManager from "ar-core-react-native"; //AR stuff
+
+import { UIManager, findNodeHandle} from 'react-native';
+import ArCoreReactNativeViewManager from "ar-core-react-native";
+
+
+// code snippit form https://www.npmjs.com/package/ar-core-react-native/v/1.2.4
+<ArCoreReactNativeViewManager ref="arCoreView" />
+// on action button or any
+// you send 2 parameter
+// name_object - type:string : name object in 3D view.
+// path_file - type:string : path file to glb in device.
+function addObject(){
+    UIManager.dispatchViewManagerCommand(
+        findNodeHandle(this.refs.arCoreView),
+        "CMD_RUN_SET_OBJECT",
+        [name_object, path_file]);
+}
+
+// on action delete
+function deleteObjectSeleted(){
+  UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.refs.arCoreView),
+      "CMD_RUN_DELETE_OBJECT",
+      []);
+}
 
 const HelloWorldApp = () => {
   return (
