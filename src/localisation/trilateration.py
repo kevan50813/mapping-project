914ef027d981400f37d5b073e6a31f4c5a87efbe
Matthew <ll17mjp@leeds.ac.uk>
2021-12-17 13:26:50 +0000

    Further changes to new LatLon format; needs work on error area estimation and plotting distance from APs

--- src/localisation/trilateration.py
+++ src/localisation/trilateration.py
@@ -1,75 +1,56 @@
 import matplotlib.pyplot as plt
-import math
+from pygeodesy.sphericalNvector import LatLon, trilaterate
+from pygeodesy.errors import IntersectionError
 
 
-def trilaterate_triplet(ap_dict, distance_dict):
+def trilaterate_triplet(ap_dict):
     """ Given 3 access point locations and distances, calculates and outputs the expected user position
 
-    :param ap_dict: Dict of 3 access points' locations, in the form { MAC: [xpos1, ypos1], ... }
-    :param distance_dict: Dict of 3 access points' distances, in the form { MAC: distance1, ... }
+    :param ap_dict: Dict of 3 access points' locations and distances from current location
 
     :return location: Array of the predicted user position
     """
 
-    # cast to array for easy access, keys do not matter here
-    ap_triplet = list(ap_dict.values())
-    distance_triplet = list(distance_dict.values())
+    pos = []
+    dist = []
 
-    # extract values for clarity
-    p1 = ap_triplet[0]
-    p2 = ap_triplet[1]
-    p3 = ap_triplet[2]
+    for key in ap_dict:
 
-    r1 = distance_triplet[0]
-    r2 = distance_triplet[1]
-    r3 = distance_triplet[2]
+        location = ap_dict[key]
+        pos.append(LatLon(location["latitude"], location["longitude"]))
 
-    # -2*x1 + 2*x2
-    a = -2*p1[0] + 2*p2[0]
+        dist.append(ap_dict[key]["distance"])
 
-    # -2*y1 + 2*y2
-    b = -2*p1[1] + 2*p2[1]
+    try:
+        pos = trilaterate(pos[0], dist[0], pos[1], dist[1], pos[2], dist[2])
+        return pos
 
-    # r1^2 - r2^2 - x1^2 + x2^2 - y1^2 + y2^2
-    c = r1**2 - r2**2 - p1[0]**2 + p2[0]**2 - p1[1]**2 + p2[1]**2
+    except IntersectionError:
+        print("ERR: Supplied points do not intersect")
+        # non-intersection
+        return -1
 
-    # -2*x2 + 2*x3
-    d = -2*p2[0] + 2*p3[0]
+    except (TypeError, ValueError):
+        # error with values
+        # TODO - handle this in a different way
+        return -1
 
-    # -2*y2 + 2*y3
-    e = -2*p2[1] + 2*p3[1]
+'''
 
-    # r2^2 - r3^2 - x2^2 + x3^2 - y2^2 + y3^2
-    f = r2**2 - r3**2 - p2[0]**2 + p3[0]**2 - p2[1]**2 + p3[1]**2
+TODO - needs updating for use with LatLon and the like
+maybe pygeodesy has a better way to do this?
 
-    # return predicted user location
-    return [(c*e - f*b)/(e*a - b*d), (c*d - a*f)/(b*d - a*e)]
-
-
-def dist(p1, p2):
-    """ Calculates distance between 2 points, each of the form [x, y]
-
-    :param p1: point 1
-    :param p2: point 2
-    :return distance: the distance between the two points, as a float
-    """
-
-    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
-
-
-def approximate_error(ap_dict, distance_dict, location):
+def approximate_error(ap_dict, ap_dict, location):
     """ Given the triplets and predicted location, approximate the crossover area of all 3 circles.
     This is then the error area - this can then be turned into a circle to give an easily measurable error metric
 
-    :param ap_dict: Access point location triplet
-    :param distance_dict: Access point distance triplet
+    :param ap_dict: Access point location + distance triplet
     :param location: Predicted location of the user
     :return error: the radius of the approximated 'region of error'. -1 if circles do not overlap
     """
 
     # cast to array for easy access, keys do not matter here
     ap_triplet = list(ap_dict.values())
-    distance_triplet = list(distance_dict.values())
 
     # the 3 points that make up the triangle bounding the intersection between the 3 circles
     triangle_points = []
@@ -118,13 +99,13 @@ def approximate_error(ap_dict, distance_dict, location):
         else:
             triangle_points.append(int2)
 
-
     t1 = triangle_points[0]     # intersection of circle 1 and 2
     t2 = triangle_points[1]     # intersection of circle 2 and 3
     t3 = triangle_points[2]     # intersection of circle 3 and 1
 
     # get area of the triangle formed by the 3 points
     area = abs(0.5 * ((t2[0] - t1[0]) * (t3[1] - t1[1]) - ((t3[0] - t1[0]) * (t2[1] - t1[1]))))
+    #print(f"Triangle area: {area}")
 
     # now take pairs of the points that make the triangle
     for index in range(3):
@@ -133,23 +114,29 @@ def approximate_error(ap_dict, distance_dict, location):
         # as they are intersections of 1+2 and 2+3
         p1 = triangle_points[index]
         p2 = triangle_points[(index + 1) % 3]
+        chord = dist(p1, p2)
         r = distance_triplet[(index + 1) % 3]
 
-        angle = math.acos(((2 * r**2) - dist(p1, p2)**2) / (2 * r**2))
+        #print(f"r: {r}, p1: {p1}, p2: {p2}, dist: {dist(p1, p2)}")
+
+        angle = math.acos(((2 * r**2) - chord**2) / (2 * r**2))
+
+        #print(f"angle: {angle}, area: {r**2 * math.asin(chord / (2 * r)) - (chord / 4) * math.sqrt(4 * r**2 - chord**2)}")
 
         # add area of segment onto total area
-        area += ((angle - math.sin(angle)) / 2) * r**2
+        area += r**2 * math.asin(chord / (2 * r)) - (chord / 4) * math.sqrt(4 * r**2 - chord**2)
 
     return math.sqrt(area / math.pi)
 
-
-def visualise_trilateration(ap_triplet, distance_triplet, location, error):
+'''
+def visualise_trilateration(ap_dict, location, error):
     """ Given 3 access point locations and distances, and the position of the user, visualises them using pyplot
     Note that the keys for the ap_triplet and distance_triplet dictionaries must be identical
 
-    :param ap_triplet: 3-length dictionary of access points' locations, in the form { MAC: [xpos, ypos], ... }
-    :param distance_triplet: 3-length dictionary of access points' distances, in the form { MAC: distance, ... }
-    :param location: Array containing the predicted location of the user, in the form [xpos, ypos]
+    X is longitude, Y is latitude
+
+    :param ap_dict: 3-length dictionary of access points' locations and distances
+    :param location: LatLon of the user's predicted position
     :param error: The radius of the circle of expected error
     """
 
@@ -157,44 +144,57 @@ def visualise_trilateration(ap_triplet, distance_triplet, location, error):
 
     # setup axis to generic values
     fig, ax = plt.subplots()
-    ax.set_xlim((0, 50))
-    ax.set_ylim((0, 50))
+    fig.tight_layout()
+
+    # keep track of maximum and minimum lon and lat found, for drawing
+    max_lat = -999
+    max_lon = -999
+    min_lat = 999
+    min_lon = 999
 
     # iterate over both dictionaries in tandem
-    for ap_key, distance_key in zip(ap_triplet, distance_triplet):
+    for key in ap_dict:
 
-        # ensure keys are consistent across each triplet
-        if ap_key != distance_key:
-            print("ERR: Key mismatch in trilateration visualisation")
-            return
+        max_lat = max(max_lat, ap_dict[key]["latitude"])
+        max_lon = max(max_lon, ap_dict[key]["longitude"])
+        min_lat = min(min_lat, ap_dict[key]["latitude"])
+        min_lon = min(min_lon, ap_dict[key]["longitude"])
 
         # create a circle from the data given
-        ap_coord = ap_triplet[ap_key]
-        distance = distance_triplet[distance_key]
-        circle = plt.Circle((ap_coord[0], ap_coord[1]), distance, fill=False)
+        # TODO - convert distance into somethign meaningful. right now plot is in lat/lon, making dist useless
+        circle = plt.Circle((ap_dict[key]["longitude"], ap_dict[key]["latitude"]), ap_dict[key]["distance"], fill=False)
 
         # plot center of circle, and add it to the plot
-        ax.plot(ap_coord[0], ap_coord[1], 'o')
-        ax.annotate(ap_key, (ap_coord[0], ap_coord[1]), textcoords="offset points", xytext=(0, 10), ha="center")
+        ax.plot(ap_dict[key]["longitude"], ap_dict[key]["latitude"], 'o')
+        ax.annotate(key, (ap_dict[key]["longitude"], ap_dict[key]["latitude"]),
+            textcoords="offset points", xytext=(0, 10), ha="center")
         ax.add_patch(circle)
 
-    # plot the user location
-    ax.plot(location[0], location[1], 'x')
+    # padding for around the APs, so they are not on the edge of the graph
+    x_padding = (max_lon - min_lon) * 0.2
+    y_padding = (max_lat - min_lat) * 0.2
 
-    if error > 0:
+    ax.set_xlim((min_lon - x_padding, max_lon + x_padding))
+    ax.set_ylim((min_lat - y_padding, max_lat + y_padding))
+
+    # plot the user location, if valid
+    if type(location) is LatLon:
+        ax.plot(location.lon, location.lat, 'x')
+
+    """
+    TODO - wait for new error calculation before plotting
+    if error is not None and error > 0:
         error_circle = plt.Circle((location[0], location[1]), error, fill=False, color="red")
         ax.add_patch(error_circle)
+        """
 
     plt.show()
 
-    print_representation(ap_triplet, distance_triplet, location, error)
 
-
-def print_representation(ap_triplet, distance_triplet, location, error):
+def print_representation(ap_dict, location, error):
     """ Repr dump for attributes of trilateration ran
 
-    :param ap_triplet
-    :param distance_triplet
+    :param ap_dict
     :param location
     :param error
     """
@@ -202,36 +202,32 @@ def print_representation(ap_triplet, distance_triplet, location, error):
     print("ACCESS POINT TRIPLET")
 
     # iterate over both dictionaries in tandem
-    for ap_key, distance_key in zip(ap_triplet, distance_triplet):
+    for ap_key in ap_dict:
 
-        # ensure keys are consistent across each triplet
-        if ap_key != distance_key:
-            print("ERR: Key mismatch in trilateration representation")
-            return
+        print(f"{ap_key}: LAT/LON <{ap_dict[ap_key]['longitude']}/"
+              f"{ap_dict[ap_key]['latitude']}>, dist {ap_dict[ap_key]['distance']}")
 
-        print(f"{ap_key}: pos <{ap_triplet[ap_key][0]}, "
-              f"{ap_triplet[ap_key][1]}>, dist {distance_triplet[distance_key]}")
-
-    print(f"\nPREDICTED LOCATION: <{location[0]}, {location[1]}>")
-    print(f"EXPECTED ERROR AREA: {error**2 * math.pi}")
-    print(f"EXPECTED ERROR RADIUS: {error}")
+    if type(location) is LatLon:
+        print(f"\nPREDICTED LAT/LON: {location.lat}/{location.lon}")
+    elif type(location) is int:
+        print(f"\nPREDICTED LOCATION: {location}")
+    
+    if error is not None:
+        print(f"EXPECTED ERROR AREA: {error**2 * math.pi}")
+        print(f"EXPECTED ERROR RADIUS: {error}")
 
 
 if __name__ == "__main__":
 
     # sample data for quick dirty test
     ap_sample = {
-        "REF_1": [25, 10],
-        "REF_2": [40, 40],
-        "REF_3": [10, 40]
+        "REF_1": {"ap_name": "ref1", "longitude": -1, "latitude": 1, "distance": 31 },
+        "REF_2": {"ap_name": "ref2", "longitude": -1.0001, "latitude": 1, "distance": 30 },
+        "REF_3": {"ap_name": "ref3", "longitude": -1, "latitude": 1.0001, "distance": 30 }
     }
 
-    distance_sample = {
-        "REF_1": 25,
-        "REF_2": 25,
-        "REF_3": 25
-    }
-
-    pos = trilaterate_triplet(ap_sample, distance_sample)
-    err = approximate_error(ap_sample, distance_sample, pos)
-    visualise_trilateration(ap_sample, distance_sample, pos, err)
+    err = None
+    pos = trilaterate_triplet(ap_sample)
+    #err = approximate_error(ap_sample, pos)
+    visualise_trilateration(ap_sample, pos, err)
+    print_representation(ap_sample, pos, err)
