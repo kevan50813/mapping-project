6528df2b202079da9a965ecac89fbeb492f4ffed
Matthew <ll17mjp@leeds.ac.uk>
2022-02-05 13:44:06 +0000

    Refactoring to match new structure

--- src/get_json.sh
+++
@@ -1,5 +0,0 @@
-#!  /bin/bash
-
-PRIVATE_TOKEN='glpat-LFvBvfu78QjuxwnY9sWr'
-
-curl -s -L --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "https://gitlab.com/api/v4/projects/31237418/jobs/artifacts/main/download?job=build-maps-job" -o temp.zip && unzip temp.zip -d maps_new && rm -rf temp.zip maps && mv maps_new maps
