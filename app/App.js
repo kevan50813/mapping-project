dd178a2983616851892904191513db73025d9f84
sc18kj <sc18kj@leeds.ac.uk>
2022-02-05 11:54:37 +0000

    added buttons and basic ui , need to add button fuctionality

--- app/App.js
+++ app/App.js
@@ -1,14 +1,74 @@
-import React, {useState} from 'react';
-import {Button, StyleSheet} from 'react-native';
-import { // evyrhing from viro, genraly if soemthing is missing and its viro related add it here
+//import React, {useState} from 'react';
+//import {Button, StyleSheet} from 'react-native';
+/*import { // evyrhing from viro, genraly if soemthing is missing and its viro related add it here
   ViroARScene,
   ViroText,
   ViroConstants,
   ViroARSceneNavigator,
   ViroBox
-} from '@viro-community/react-viro';
+} from '@viro-community/react-viro';*/
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-
+ import React from 'react';
+ import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+ 
+ const Separator = () => (
+   <View style={styles.separator} />
+ );
+ 
+ const App = () => (
+   <SafeAreaView style={styles.container}>
+     <View>
+       <Button
+         title="localization"
+         onPress={() => Alert.alert('Simple Button pressed')}
+       />
+     </View>
+     <Separator />
+     <View>
+       <Button
+         title="mapping"
+         onPress={() => Alert.alert('Simple Button pressed')}
+       />
+     </View>
+     <Separator />
+     <View>
+       <Button
+         title="server"
+         onPress={() => Alert.alert('Simple Button pressed')}
+       />
+     </View>
+   </SafeAreaView>
+ );
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     justifyContent: 'center',
+     marginHorizontal: 16,
+   },
+   title: {
+     textAlign: 'center',
+     marginVertical: 8,
+   },
+   fixToText: {
+     flexDirection: 'row',
+     justifyContent: 'space-between',
+   },
+   separator: {
+     marginVertical: 8,
+     borderBottomColor: '#737373',
+     borderBottomWidth: StyleSheet.hairlineWidth,
+   },
+ });
+ 
+ export default App;
 /* COMMETED OUT UNTIL WE NEED AR STUFF
 // all AR related stuff from vriomidea timeplate
 const InitialScene=()=>{
