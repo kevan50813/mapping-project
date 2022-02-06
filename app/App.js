d233bf55e5c3221f5bdebc799a1343cf09326f97
sc18kj <sc18kj@leeds.ac.uk>
2022-02-06 13:59:03 +0000

    split js files acorss seperte files

--- app/App.js
+++ app/App.js
@@ -19,110 +19,10 @@
  import { View, Text, StyleSheet, SafeAreaView, Button, Separator } from 'react-native';
  import { NavigationContainer } from '@react-navigation/native';
  import { createStackNavigator } from '@react-navigation/stack';
- 
-/* each fuction is basicly its own screen chage which screen applied to your project directly */
-
- function HomeScreen({ navigation }) {
-   return (
-      <SafeAreaView style={styles.container}>
-        <View>
-          <Button
-            title="Localization"
-            onPress={() => navigation.navigate('Localization')}
-          />
-        </View>
-
-        <View>
-          <Button
-            title="Mapping"
-            onPress={() => navigation.navigate('Mapping')}
-          />
-        </View>
-
-        <View>
-          <Button
-            title="Server"
-            onPress={() => navigation.navigate('Server')}
-          />
-        </View>
-    </SafeAreaView>
-   );
- }
- 
- function LocalizationScreen({ navigation }) {
-   return (
-    <SafeAreaView style={styles.container}>
-    <View>
-      <Button
-        title="Home"
-        onPress={() => navigation.navigate('Home')}
-      />
-    </View>
-    <View>
-      <Button
-        title="Mapping"
-        onPress={() => navigation.navigate('Mapping')}
-      />
-    </View>
-    <View>
-      <Button
-        title="Server"
-        onPress={() => navigation.navigate('Server')}
-      />
-    </View>
-  </SafeAreaView>
-   );
- }
-
- function MapScreen({ navigation }) {
-  return (
-   <SafeAreaView style={styles.container}>
-   <View>
-     <Button
-       title="Home"
-       onPress={() => navigation.navigate('Home')}
-     />
-   </View>
-   <View>
-     <Button
-       title="Localization"
-       onPress={() => navigation.navigate('Localization')}
-     />
-   </View>
-   <View>
-     <Button
-       title="Server"
-       onPress={() => navigation.navigate('Server')}
-     />
-   </View>
- </SafeAreaView>
-  );
-}
-
-function ServerScreen({ navigation }) {
-  return (
-   <SafeAreaView style={styles.container}>
-   <View>
-     <Button
-       title="Home"
-       onPress={() => navigation.navigate('Home')}
-     />
-   </View>
-   <View>
-     <Button
-       title="Mapping"
-       onPress={() => navigation.navigate('Mapping')}
-     />
-   </View>
-   <View>
-     <Button
-       title="Localization"
-       onPress={() => navigation.navigate('Localization')}
-     />
-   </View>
- </SafeAreaView>
-  );
-}
+ import { HomeScreen } from './screens/HomeScreen.js';
+ import { MapScreen } from './screens/MapScreen.js';
+ import { ServerScreen } from './screens/ServerScreen.js';
+ import { LocalizationScreen } from './screens/LocalizationScreen.js';
  
  const Stack = createStackNavigator();
  
@@ -138,27 +38,6 @@ function ServerScreen({ navigation }) {
      </NavigationContainer>
    );
  }
-
- const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    marginHorizontal: 16,
-  },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-});
  
  export default App;
  
