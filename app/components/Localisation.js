2d7078be965a849dad43a018f90c7270e0136fa2
Matthew <ll17mjp@leeds.ac.uk>
2022-02-08 12:02:19 +0000

    Beginning work on trilaterion implementation

---
+++ app/components/Localisation.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { ScrollView, Text, View, PermissionsAndroid } from 'react-native';
+import WifiManager from 'react-native-wifi-reborn';
+import { Button } from './Button';
+import { styles } from './styles';
+
+export const Localisation = () => {
+
+    return (
+        <Text>wip</Text>
+    )
+};
