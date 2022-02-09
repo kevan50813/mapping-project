9f619c4882c7ac1054b750fdd04962c7e4971434
sc18kj <sc18kj@leeds.ac.uk>
2022-02-09 12:02:31 +0000

    grpah showing, but not rendering points

--- app/components/Trilateration.js
+++ app/components/Trilateration.js
@@ -1,33 +1,22 @@
 import React from 'react';
 import { ScrollView, View } from 'react-native';
 import { styles } from './styles';
-import Plot from 'react-native-plotly';
+import Plotly from 'react-native-plotly';
+
 
 export const Trilateration = () => {
-  const data = {
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 3, 4, 8],
-    type: 'scatter',
-  };
-  const layout = { width: 500, height: 500, title: 'Trilateration scatter plot' ,
-    xaxis: {
-      title: 'LAT',
-    },
-    yaxis: {
-      title: 'LON',
-    }
-  };
+    const data = {
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 3, 4, 8],
+      type: 'scatter',
+    };
+    const layout = { title: 'Trilateration Scatterplot' };
 
-  return (
-    <View style={styles.background}>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Plot
-          data={data}
-          layout={layout}
-          onLoad={() => console.log('graph loaded')}
-        />
-      </ScrollView>
-    </View>
-  );
-  
+    return (
+      <Plotly
+        data={data}
+        layout={layout}
+      />
+    )
+
 };
