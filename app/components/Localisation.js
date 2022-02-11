276548946c9b3dbb0cf157ee219b0dbdb0435152
Matthew <ll17mjp@leeds.ac.uk>
2022-02-11 09:58:19 +0000

    Implemented useEffect to await useState update, now can work on merging scanned + JSON network data

--- app/components/Localisation.js
+++ app/components/Localisation.js
@@ -1,26 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, Text, View } from 'react-native';
 import { Button } from './Button';
 import { styles } from './styles';
 import { Scan } from './Scanner';
 
 export const Localisation = () => {
-  const [networks, setNetworks] = useState([]);
-  const [scanning, setScanning] = useState(false);
-  const [error, setError] = useState('');
-  const [time, setTime] = useState({ start: new Date(), end: new Date() });
+  const [networkScanned, setNetworkScanned] = useState([]);
   const [networkData, setNetworkData] = useState([]);
+  const [networkWithDist, setNetworkWithDist] = useState([]);
 
-  const startScan = async () => {
-    setScanning(true);
-
+  const execute = async () => {
     let scan = new Scan();
     await scan.startScan();
+    setNetworkScanned(scan.getNetworks());
 
-    setNetworks(scan.getNetworks());
-    setError(scan.getError());
-    setTime(scan.getTime());
-    setScanning(false);
+    await loadData();
+
+    // any further execution now is in useEffect when both arrays have been populated
   };
 
   const loadData = async () => {
@@ -33,18 +29,28 @@ export const Localisation = () => {
     );
   };
 
+  // only fire when networkData/networkScanned update
+  useEffect(() => {
+    if (networkData.length > 0 && networkScanned.length > 0) {
+      console.log(networkData);
+      console.log(networkScanned);
+    }
+  }, [networkData, networkScanned]);
+
   return (
     <View style={styles.background}>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <Button
           style={styles.button}
-          title="Load JSON Data"
-          onPress={loadData}
+          title="Execute Process"
+          onPress={execute}
         />
-        {networkData !== null ? (
+        {networkData.length > 0 ? (
           <Text style={styles.info}>Loaded network data from JSON.</Text>
         ) : null}
-        <Button style={styles.button} title="Scan" onPress={startScan} />
+        {networkScanned > 0 ? (
+          <Text style={styles.info}>Network scan successful.</Text>
+        ) : null}
       </ScrollView>
     </View>
   );
